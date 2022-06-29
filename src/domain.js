579283a68306cd43d7e811dfeebb92fe1f3e02e0
Cyberspace Scripts <102620537+CyberspaceScripts@users.noreply.github.com>
2022-06-29 20:01:18 +0530

    recharge

--- src/domain.js
+++ src/domain.js
@@ -1,3 +1,2 @@
-export const serverDomain = 'https://royal-server-3.herokuapp.com/';
-// export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
-// export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
+// export const serverDomain = 'https://royal-server-3.herokuapp.com/';
+export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
