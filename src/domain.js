11ba80a1360ba51d199c7d3fb0566257ff874217
Harsh <73060745+Harsh9466@users.noreply.github.com>
2022-08-24 12:18:06 +0530

    Fixing Server

--- src/domain.js
+++ src/domain.js
@@ -1,3 +1,6 @@
+// Server 1
 // export const serverDomain = 'https://royal-server-3.herokuapp.com/';
-// export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
+// Server 2
+export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
+// Break Server
 // export const serverDomain = 'https://royal-satta-server.heroku.com/';
