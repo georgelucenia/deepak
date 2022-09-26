66f46550e22569ce7ecf1f2c7adee9f1fd1aa4a8
Harsh <73060745+Harsh9466@users.noreply.github.com>
2022-09-26 11:47:23 +0530

    fdsaf

--- src/domain.js
+++ src/domain.js
@@ -1,6 +1,6 @@
 // Server 1
 // export const serverDomain = 'https://royal-server-3.herokuapp.com/';
 // Server 2
-export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
+// export const serverDomain = 'https://royal-satta-server.herokuapp.com/';
 // Break Server
 // export const serverDomain = 'https://royal-satta-server.heroku.com/';
